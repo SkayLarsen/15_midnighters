d5db8d27715d25493c8ca0697a9fc02aaa429891
SkayLarsen <SkayLarsen@gmail.com>
2017-01-07 18:31:01 +0300

    задача #15

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Скрипт выводит список учащихся на devman, которые отправляли задачи на проверку после 24:00
+
+# Quickstart
+
+Скрипт требует для своей работы установленный интерпретатор Python версии 3.5, а также некоторые дополнительные зависимости, которые можно установить следующим образом:
+```
+pip install -r requirements.txt
+```
+
+Запуск на Linux:
+
+```#!bash
+$ python seek_dev_nighters.py
+```
+Запуск на Windows происходит аналогично.
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,29 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+from datetime import time, datetime
+
+
+def load_attempts(pages=5):
+    url = 'https://devman.org/api/challenges/solution_attempts'
+    for page in range(1, pages + 1):
+        response = requests.get(url, params={'page': page}).json()
+        for record in response['records']:
+            yield record
+
+
+def check_midnighter(record, end_hour=3):
+    timestamp = record['timestamp']
+    if timestamp:
+        timezone = record['timezone']
+        attempt_time = datetime.fromtimestamp(timestamp, pytz.timezone(timezone)).time()
+        return time(0, 0) < attempt_time < time(end_hour, 0)
+
 
 def get_midnighters():
-    pass
+    return {record['username'] for record in load_attempts() if check_midnighter(record)}
+
 
 if __name__ == '__main__':
-  pass
+    midnighters = get_midnighters()
+    print("Отправляли задачи на проверку после полуночи:")
+    print('\n'.join(midnighters))
